3e3901ae82be093b819d9e3861e05627cb4c0d2b
Takahiro Makabe <guidazhenbi41@gmail.com>
2026-05-06 18:46:05 +0900

    feat: implement reservation api (#52)

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -2,4 +2,8 @@ pub mod router;
 pub mod state;
 pub mod handlers {
     pub mod health;
+    pub mod reservation;
+}
+pub mod dto{
+    pub mod reservation;
 }
--- src/api/router.rs
+++ src/api/router.rs
@@ -1,9 +1,21 @@
 use axum::{
-    routing::get,
+    routing::{
+        get,
+        post,
+        patch,
+        delete,
+    },
     Router,
 };
 
 use crate::api::handlers::health::health;
+
+use crate::api::handlers::reservation::{
+    create_reservation,
+    modify_reservation,
+    cancel_reservation,
+};
+
 use crate::api::state::AppState;
 
 pub fn create_router(
@@ -11,6 +23,26 @@ pub fn create_router(
 ) -> Router {
 
     Router::new()
-        .route("/health", get(health))
+
+        .route(
+            "/health",
+            get(health),
+        )
+
+        .route(
+            "/reservations",
+            post(create_reservation),
+        )
+
+        .route(
+            "/reservations/:id",
+            patch(modify_reservation),
+        )
+
+        .route(
+            "/reservations/:id",
+            delete(cancel_reservation),
+        )
+
         .with_state(state)
 }
